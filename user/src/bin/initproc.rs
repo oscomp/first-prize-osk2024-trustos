608f5d256394d8ef35928de7191bc84ea951be43
13eholder <925349624@qq.com>
2024-03-27 17:07:44 +0800

    fix:删除不必要的yield_()
    Former-commit-id: 4c274ccf46d46a2feef57bfbcb40a2ff30df0b77

--- user/src/bin/initproc.rs
+++ user/src/bin/initproc.rs
@@ -4,7 +4,7 @@
 #[macro_use]
 extern crate user_lib;
 
-use user_lib::{exec, fork, wait, yield_};
+use user_lib::{exec, fork, wait};
 
 #[no_mangle]
 fn main() -> i32 {
@@ -15,10 +15,6 @@ fn main() -> i32 {
         loop {
             let mut exit_code: i32 = 0;
             let pid = wait(&mut exit_code);
-            if pid == -1 {
-                yield_();
-                continue;
-            }
             println!(
                 "[initproc] Released a zombie process, pid={}, exit_code={}",
                 pid, exit_code,
