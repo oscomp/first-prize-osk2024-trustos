e0a41dc13646fa426891e66ff06c75859f4b6ee6
13eholder <925349624@qq.com>
2024-05-08 18:33:27 +0800

    feat: 回调initproc
    Former-commit-id: ca78cbe8837a47e9601e900947c0dfa0b19fda2c

--- user/src/bin/initproc.rs
+++ user/src/bin/initproc.rs
@@ -4,15 +4,13 @@
 #[macro_use]
 extern crate user_lib;
 
-use user_lib::{exec, fork, run_busyboxsh, wait};
+use user_lib::{exec, fork, wait};
 
 #[no_mangle]
 fn main() -> i32 {
     println!("initproc running...");
     if fork() == 0 {
-        // exec("user_shell\0");
-        // exec("/rCoretests/hello_world\0");
-        run_busyboxsh();
+        exec("user_shell\0");
     } else {
         loop {
             let mut exit_code: i32 = 0;
