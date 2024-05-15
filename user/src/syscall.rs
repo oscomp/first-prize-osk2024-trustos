7510bc3ee02768fc1bddeaf47b77f27562a5d715
windflower <windflower_weng@qq.com>
2024-05-15 16:43:14 +0800

    将busybox的编译加入内核的Makefile便于调试，Makefile提供了run、srun、brun三个选项，详见Makefile开头注释
    Former-commit-id: 7e3848e1a6cff16cc89f8a03f19342afe9912bf7

--- user/src/syscall.rs
+++ user/src/syscall.rs
@@ -156,7 +156,7 @@ pub fn sys_busyboxsh() -> isize {
             [
                 "busybox\0".as_ptr() as isize,
                 "ls\0".as_ptr() as isize,
-                "/\0".as_ptr() as isize,
+                "/rCoretests\0".as_ptr() as isize,
                 0,
             ]
             .as_ptr() as isize,
