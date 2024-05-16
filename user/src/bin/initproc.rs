8003552b6546b341899a8e5b9e1979f4f98d4b7a
13eholder <925349624@qq.com>
2024-05-16 13:44:45 +0800

    style:移除无用的初赛测试
    Former-commit-id: 5127436c17ebab1fc20b9ebe040f057917cb42ec

--- user/src/bin/initproc.rs
+++ user/src/bin/initproc.rs
@@ -10,13 +10,13 @@ use user_lib::{exec, fork, run_busyboxsh, sleep, wait};
 fn main() -> i32 {
     println!("initproc running...");
     if fork() == 0 {
-        //exec("user_shell\0");
-        run_busyboxsh();
+        exec("user_shell\0");
+        // run_busyboxsh();
     } else {
         loop {
             let mut exit_code: i32 = 0;
             let pid = wait(&mut exit_code);
-            sleep(5000);
+            // sleep(5000);
             println!(
                 "[initproc] Released a zombie process, pid={}, exit_code={}",
                 pid, exit_code,
