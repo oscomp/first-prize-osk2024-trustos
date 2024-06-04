648edc5123eae028ae575f6fe502e649181ed74c
brightnight <brightnight@934111946@qq.com>
2024-06-04 16:09:43 +0800

    fix:修复了dirent的名字数组问题导致的重复搜索问题，现在可以通过du和find的测试
    Former-commit-id: bf8fcf4e5aa72be4c8d149b1b44aa7d777f6864b

--- os/src/fs/dirent.rs
+++ os/src/fs/dirent.rs
@@ -4,11 +4,11 @@ pub const NAME_LIMIT: usize = 253;
 #[repr(C)]
 #[derive(Debug)]
 pub struct Dirent {
-    d_ino: usize,             // 索引节点号
-    d_off: isize,             // 从 0 开始到下一个 dirent 的偏移
-    d_reclen: u16,            // 当前 dirent 的长度
-    d_type: u8,               // 文件类型
-    d_name: [u8; NAME_LIMIT], // 文件名
+    d_ino: usize,    // 索引节点号
+    d_off: isize,    // 从 0 开始到下一个 dirent 的偏移
+    d_reclen: u16,   // 当前 dirent 的长度
+    d_type: u8,      // 文件类型
+    d_name: [u8; 0], // 文件名
 }
 
 impl Dirent {
@@ -18,7 +18,7 @@ impl Dirent {
             d_off: 0,
             d_reclen: core::mem::size_of::<Self>() as u16,
             d_type: 0,
-            d_name: [0; NAME_LIMIT],
+            d_name: [0; 0],
         }
     }
 
@@ -34,16 +34,26 @@ impl Dirent {
     }
 
     fn fill_name(&mut self, name: &str) {
+        /*
         let len = name.len().min(NAME_LIMIT);
         let name_bytes = name.as_bytes();
         for i in 0..len {
             self.d_name[i] = name_bytes[i];
         }
         self.d_name[len] = 0;
+        */
+        let name_bytes = name.as_bytes();
+        self.d_reclen = (core::mem::size_of::<Self>() + name_bytes.len() + 1) as u16;
+        let name_vec = unsafe {
+            core::slice::from_raw_parts_mut(self.d_name.as_mut_ptr(), name_bytes.len() + 1)
+        };
+        name_vec[..name_bytes.len()].copy_from_slice(name_bytes);
+        name_vec[name_bytes.len()] = b'\0';
     }
 
     pub fn as_bytes(&self) -> &[u8] {
-        let size = core::mem::size_of::<Self>();
+        //特殊处理，因为名字数组大小不定
+        let size = self.len() as usize;
         unsafe { core::slice::from_raw_parts(self as *const _ as usize as *const u8, size) }
     }
 }
--- os/src/fs/stat.rs
+++ os/src/fs/stat.rs
@@ -8,17 +8,17 @@ const S_IFLINK: u32 = 0xA000;
 #[repr(C)]
 #[derive(Debug)]
 pub struct Kstat {
-    st_dev: u32,   // 包含文件的设备 ID
+    st_dev: u64,   // 包含文件的设备 ID
     st_ino: u64,   // 索引节点号
     st_mode: u32,  // 文件类型和模式
     st_nlink: u32, // 硬链接数
     st_uid: u32,   // 所有者的用户 ID
     st_gid: u32,   // 所有者的组 ID
-    st_rdev: u32,  // 设备 ID（如果是特殊文件）
+    st_rdev: u64,  // 设备 ID（如果是特殊文件）
     __pad: u64,
-    st_size: i64,    // 总大小，以字节为单位
-    st_blksize: i64, // 文件系统 I/O 的块大小
-    __pad2: i32,
+    st_size: u64,    // 总大小，以字节为单位
+    st_blksize: u32, // 文件系统 I/O 的块大小
+    __pad2: u32,
     st_blocks: u64,     // 分配的 512B 块数
     st_atime_sec: i64,  // 上次访问时间
     st_atime_nsec: i64, // 上次访问时间（纳秒精度）
@@ -26,7 +26,6 @@ pub struct Kstat {
     st_mtime_nsec: i64, // 上次修改时间（纳秒精度）
     st_ctime_sec: i64,  // 上次状态变化的时间
     st_ctime_nsec: i64, // 上次状态变化的时间（纳秒精度）
-    __unused: [u32; 2],
 }
 
 impl Kstat {
@@ -50,13 +49,12 @@ impl Kstat {
             st_mtime_nsec: 0,
             st_ctime_sec: 0,
             st_ctime_nsec: 0,
-            __unused: [0; 2],
         }
     }
     pub fn init(
         &mut self,
         st_ino: u64,
-        st_size: i64,
+        st_size: u64,
         st_atime: i64,
         st_mtime: i64,
         st_ctime: i64,
