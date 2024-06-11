9e739beb251eafc58a11d203ec9ee5e891030c29
13eholder <925349624@qq.com>
2024-06-11 10:49:11 +0800

    style:重构Driver
    Former-commit-id: 2b94bf94ee42b4d14df6c4b9279e9d0dc71aa7b6

--- os/src/boards/qemu.rs
+++ os/src/boards/qemu.rs
@@ -6,5 +6,3 @@ pub const MMIO: &[(usize, usize)] = &[
     (0x0010_0000, 0x00_2000), // VIRT_TEST/RTC  in virt machine
     (0x1000_1000, 0x00_1000), // Virtio Block in virt machine
 ];
-
-pub type BlockDeviceImpl = crate::drivers::block::VirtIOBlock;
--- os/src/fs/fat32/mod.rs
+++ os/src/fs/fat32/mod.rs
@@ -1,15 +1,18 @@
 mod inode;
 mod sb;
 
+use core::cell::RefCell;
+
 use alloc::sync::Arc;
-use fat32_fs::{create_root_vfile, FAT32Manager, VFile};
+use fat32_fs::{create_root_vfile, BlockDevice, FAT32Manager, VFile, BLOCK_SZ};
 pub use inode::*;
 use lazy_static::*;
 pub use sb::*;
 
 use crate::{
-    drivers::BLOCK_DEVICE,
+    drivers::{BlockDeviceImpl, Disk},
     fs::{insert_vfile_idx, Inode, SuperBlock},
+    sync::SyncUnsafeCell,
 };
 
 use super::OSInode;
@@ -28,9 +31,30 @@ impl OSInode {
     }
 }
 
+pub struct DiskAdapter {
+    inner: SyncUnsafeCell<Disk>,
+}
+
+impl BlockDevice for DiskAdapter {
+    fn read_block(&self, block_id: usize, buf: &mut [u8]) {
+        let mut disk = self.inner.get_unchecked_mut();
+        disk.set_position(block_id * BLOCK_SZ);
+        disk.read_one(buf);
+    }
+
+    fn write_block(&self, block_id: usize, buf: &[u8]) {
+        let mut disk = self.inner.get_unchecked_mut();
+        disk.set_position(block_id * BLOCK_SZ);
+        disk.write_one(buf);
+    }
+}
+
 lazy_static! {
+    static ref DISK_ADAPTER: Arc<DiskAdapter> = Arc::new(DiskAdapter {
+        inner: SyncUnsafeCell::new(Disk::new(BlockDeviceImpl::new_device())),
+    });
     pub static ref SUPER_BLOCK: Arc<dyn SuperBlock> = {
-        let fat32_manager = FAT32Manager::open(BLOCK_DEVICE.clone());
+        let fat32_manager = FAT32Manager::open(DISK_ADAPTER.clone());
         let root = Arc::new(create_root_vfile(&fat32_manager));
         Arc::new(FATSuperBlock::new(fat32_manager, root))
     };
