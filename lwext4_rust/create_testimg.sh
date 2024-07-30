c9b906b50665218e5494abd9d59d0c472d7115b6
windflower <windflower_weng@qq.com>
2024-07-30 21:30:33 +0800

    fix:再试试
    Former-commit-id: 5f4ae8546905ed5e5238e39f9b03491709988416

--- lwext4_rust/create_testimg.sh
+++ lwext4_rust/create_testimg.sh
@@ -4,10 +4,10 @@ dd if=/dev/zero of=ext4.img bs=1M count=1
 DIR=lwext4_rust
 
 # 格式化为 ext4
-sudo mkfs.ext4 ext4.img
-sudo chmod 777 ext4.img
-sudo mkdir ../${DIR}/fs 
-sudo mount ../${DIR}/ext4.img ../${DIR}/fs
-sudo cp ../final_tests/test_all_1stage.sh ../${DIR}/fs/ 
-sudo umount ../${DIR}/fs
-sudo rmdir ../${DIR}/fs
+mkfs.ext4 ext4.img
+chmod 777 ext4.img
+mkdir ../${DIR}/fs 
+guestmount -a ../${DIR}/ext4.img -m /dev/sda --rw ../${DIR}/fs
+cp ../final_tests/test_all_1stage.sh ../${DIR}/fs/ 
+guestunmount ../${DIR}/fs
+rmdir ../${DIR}/fs
