085c923529b74a599b4bb65492662add0052963a
windflower <windflower_weng@qq.com>
2024-07-30 20:25:17 +0800

    feat:自定义测例
    Former-commit-id: 6bfe7455eec3d7c73206c6570dc5e118b3b098a8

---
+++ lwext4_rust/create_testimg.sh
@@ -0,0 +1,13 @@
+# 制作一个全0的镜像文件
+dd if=/dev/zero of=ext4.img bs=1M count=1
+
+DIR=lwext4_rust
+
+# 格式化为 ext4
+sudo mkfs.ext4 ext4.img
+sudo chmod 777 ext4.img
+sudo mkdir ../${DIR}/fs 
+guestmount -a ../${DIR}/ext4.img -i --rw ../${DIR}/fs
+sudo cp ../final_tests/scripts/test_all.sh ../${DIR}/fs/ 
+guestunmount ../${DIR}/fs
+sudo rmdir ../${DIR}/fs
